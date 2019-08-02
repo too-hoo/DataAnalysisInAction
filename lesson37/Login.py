487da19a37f20aee5431bb2df07a8034fcdd2344
too-hoo <13414851554@163.com>
2019-08-02 23:49:18 +0800

    使用selenium自动化运营微博

---
+++ lesson37/Login.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+import time
+from selenium import webdriver
+browser = webdriver.Chrome("/home/toohoo/Desktop/chromedriver_linux64/chromedriver")
+# 登录微博
+def weibo_login(username, password):
+     # 打开微博登录页
+     browser.get('https://passport.weibo.cn/signin/login')
+     browser.implicitly_wait(5)
+     time.sleep(1)
+     # 填写登录信息：用户名、密码
+     browser.find_element_by_id("loginName").send_keys(username)
+     browser.find_element_by_id("loginPassword").send_keys(password)
+     time.sleep(1)
+     # 点击登录
+     browser.find_element_by_id("loginAction").click()
+     time.sleep(1)
+# 设置用户名、密码
+username = '15218218255'
+password = "Wb13414851554"
+weibo_login(username, password)
+
+
